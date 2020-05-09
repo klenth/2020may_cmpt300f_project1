098901a5bc854ecb7b1396bff831704d88a5ac42
Kathy Lenth <klenth@westminstercollege.edu>
2020-05-09 09:29:37 -0600

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "2020may_cmpt300f_project1"
+
+version := "0.1"
+
+scalaVersion := "2.13.2"
---
+++ src/main/scala/HelloScalaWorld.scala
@@ -0,0 +1,5 @@
+object HelloScalaWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, Scala world!")
+  }
+}
